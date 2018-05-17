6b4cb911a60d7854252c29595cf2414c6606426a
chengjiwen <chengjiwen@fangjia.com>
2018-05-17 10:52:34 +0800

    'produce'

--- article_producer.py
+++ article_producer.py
@@ -1,8 +1,10 @@
 from fenghuang.fenghuang_producer import Fenghuang
 from wangyi.wangyi_producer import Wangyi
+from fangtianxia.fangtianxia_start import fangtianxia_start
 
 if __name__ == '__main__':
     fen = Fenghuang()
     fen.start_crawler()
     # wangyi = Wangyi()
     # wangyi.start_crawler()
+    fangtianxia_start()
