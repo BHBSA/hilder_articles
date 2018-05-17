599ab3198dc927a41c945c953f836fd1c040ccfd
chengjiwen <chengjiwen@fangjia.com>
2018-05-17 14:03:11 +0800

    'qiniu'

--- article_img/qiniu_fetch.py
+++ article_img/qiniu_fetch.py
@@ -45,19 +45,19 @@ def qiniufetch(url,file_name):
         "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.146 Safari/537.36"
     }
     if 'http://' in file_name:
-        # image_download = Proxy_contact(app_name='qiniufetch', method='get', url=url, headers=headers)
-        # con = image_download.contact()
-        while True:
-            try:
-                proxy = proxies[random.randint(0, 9)]
-                res = requests.get(url, headers=headers, proxies=proxy, timeout=10)  # 图片连接请求
-                con = res.content
-                if res.status_code == 200:
-                    break
-                else:
-                    continue
-            except Exception as e:
-                print(e)
+        image_download = Proxy_contact(app_name='qiniufetch', method='get', url=url, headers=headers)
+        con = image_download.contact()
+        # while True:
+        #     try:
+        #         proxy = proxies[random.randint(0, 9)]
+        #         res = requests.get(url, headers=headers, proxies=proxy, timeout=10)  # 图片连接请求
+        #         con = res.content
+        #         if res.status_code == 200:
+        #             break
+        #         else:
+        #             continue
+        #     except Exception as e:
+        #         print(e)
         # if con == False:
         #     return None
         filename_1 = file_name.replace('http://','')
