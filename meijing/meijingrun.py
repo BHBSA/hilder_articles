dfc08d090a833d6d1e1a303dda62a0fc8140280c
zjx <zjx@fangtianxiang.com>
2018-05-18 18:21:49 +0800

    'new'

--- meijing/meijingrun.py
+++ meijing/meijingrun.py
@@ -16,6 +16,7 @@ import datetime
 import re
 import random
 from article_img.image_replace import ImageReplace
+import time
 
 setting = yaml.load(open('config_local.yaml'))
 article = Article('每经')
@@ -43,80 +44,80 @@ class Meijing(object):
             piece = soup.select('.m-columnnews-list')[0]
             eachpiece = piece.select('li')
             for i in eachpiece:
-                    read_num = i.select('.f-source > span')[2].text.strip().strip('阅读')               #阅读量
-                    link = i.select('.f-title')[0].get('href')                                          #链接
-                    if self.bf.is_contains(link):
-                        print('bloom_filter已经存在{}'.format(link))
-                    else:
-                        self.bf.insert(link)
-                        print('bloom_filter不存在，插入新的url:{}'.format(link))
-                        proxies = [{"http": "http://192.168.0.96:3234"},
-                                   {"http": "http://192.168.0.93:3234"},
-                                   {"http": "http://192.168.0.90:3234"},
-                                   {"http": "http://192.168.0.94:3234"},
-                                   {"http": "http://192.168.0.98:3234"},
-                                   {"http": "http://192.168.0.99:3234"},
-                                   {"http": "http://192.168.0.100:3234"},
-                                   {"http": "http://192.168.0.101:3234"},
-                                   {"http": "http://192.168.0.102:3234"},
-                                   {"http": "http://192.168.0.103:3234"}, ]
-                        headers = {
-                            'Accept': 'text / html, application / xhtml + xml, application / xml;q = 0.9, image / webp, image / apng, * / *;q = 0.8',
-                            'Accept - Encoding': 'gzip, deflate',
-                            'Accept - Language': 'zh - CN, zh;q = 0.9',
-                            'Cache - Control': 'max - age = 0',
-                            'Connection': 'keep - alive'
-                        }
-                        while True:
-                            try:
-                                response = requests.get(url=link, headers=headers, proxies=proxies[random.randint(0, 9)])
-                                break
-                            except Exception as e:
-                                print(e)
-
-                        soup1 = BeautifulSoup(response.text, 'lxml')
-
-                        title = soup1.select('.g-article-top > h1')[0].text.strip()
-                        source = soup1.select('.source')[0].text.strip()
-                        time = soup1.select('.time')[0].text.strip()
-                        content = soup1.select('.g-articl-text')[0]
-                        content = content.prettify()
-                        img_replace = ImageReplace()
-                        con = img_replace.image_download(content)
-                        tag = soup1.select('.u-aticle-tag > span')
-                        category = soup1.select('.u-column')[0].text
-                        L = []
-                        for j in tag:
-                            tagList = j.text
-                            L.append(tagList)
-
+                read_num = i.select('.f-source > span')[2].text.strip().strip('阅读')               #阅读量
+                link = i.select('.f-title')[0].get('href')                                          #链接
+                if self.bf.is_contains(link):
+                    print('bloom_filter已经存在{}'.format(link))
+                else:
+                    self.bf.insert(link)
+                    print('bloom_filter不存在，插入新的url:{}'.format(link))
+                    proxies = [{"http": "http://192.168.0.96:3234"},
+                               {"http": "http://192.168.0.93:3234"},
+                               {"http": "http://192.168.0.90:3234"},
+                               {"http": "http://192.168.0.94:3234"},
+                               {"http": "http://192.168.0.98:3234"},
+                               {"http": "http://192.168.0.99:3234"},
+                               {"http": "http://192.168.0.100:3234"},
+                               {"http": "http://192.168.0.101:3234"},
+                               {"http": "http://192.168.0.102:3234"},
+                               {"http": "http://192.168.0.103:3234"}]
+                    headers = {
+                        'Accept': 'text / html, application / xhtml + xml, application / xml;q = 0.9, image / webp, image / apng, * / *;q = 0.8',
+                        'Accept - Encoding': 'gzip, deflate',
+                        'Accept - Language': 'zh - CN, zh;q = 0.9',
+                        'Cache - Control': 'max - age = 0',
+                        'Connection': 'keep - alive',
+                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.146 Safari/537.36'
+                    }
+                    while True:
                         try:
-                            desc = soup1.select('.g-article-abstract > p')[0].text
-                            article.desc = desc
-                            imglink = i.select('.u-columnnews-img > img')[0].get('data-aload')  # 图片链接
-                            file_name = imglink
-                            imglink = qiniufetch(imglink, file_name)
-                            article.title_img = imglink
-                        except:
-                            article.desc = ''
-                            article.title_img = ''
-                        article.title = title
-                        article.source_detail = source
-                        article.post_time = time
-                        article.body = con
-                        article.tag = L
-                        article.category = category
-                        article.read_num = read_num
-                        article.url = link
-                        article.crawler_time = datetime.datetime.now()
-                        article.insert_db()
+                            response = requests.get(url=link, headers=headers, proxies=proxies[random.randint(0, 9)])
+                            break
+                        except Exception as e:
+                            print(e)
+
+                    soup1 = BeautifulSoup(response.text, 'lxml')
+
+                    title = soup1.select('.g-article-top > h1')[0].text.strip()
+                    source = soup1.select('.source')[0].text.strip()
+                    time = soup1.select('.time')[0].text.strip()
+                    content = soup1.select('.g-articl-text')[0]
+                    content = content.prettify()
+                    img_replace = ImageReplace()
+                    con = img_replace.image_download(content)
+                    tag = soup1.select('.u-aticle-tag > span')
+                    category = soup1.select('.u-column')[0].text
+                    L = []
+                    for j in tag:
+                        tagList = j.text
+                        L.append(tagList)
+
+                    try:
+                        desc = soup1.select('.g-article-abstract > p')[0].text
+                        article.desc = desc
+                        imglink = i.select('.u-columnnews-img > img')[0].get('data-aload')  # 图片链接
+                        file_name = imglink
+                        imglink = qiniufetch(imglink, file_name)
+                        article.title_img = imglink
+                    except Exception as e:
+                        print(e)
+                    article.title = title
+                    article.source_detail = source
+                    article.post_time = time
+                    article.body = con
+                    article.tag = L
+                    article.category = category
+                    article.read_num = read_num
+                    article.url = link
+                    article.crawler_time = datetime.datetime.now()
+                    article.insert_db()
+
+
 
             morelink = soup.select('#more')[0].get('href')
             return morelink
         except Exception as e:
             print(e)
-
-
 
 def more(url):
     try:
@@ -132,20 +133,37 @@ def more(url):
                 'X-CSRF-Token':'+qYhSliyi8ZV2tLBI+HES72WeuOwOkp1yIP6A+8SgLk=',
                 'X-Requested-With':'XMLHttpRequest'
                 }
+        proxies = [{"http": "http://192.168.0.96:3234"},
+                   {"http": "http://192.168.0.93:3234"},
+                   {"http": "http://192.168.0.90:3234"},
+                   {"http": "http://192.168.0.94:3234"},
+                   {"http": "http://192.168.0.98:3234"},
+                   {"http": "http://192.168.0.99:3234"},
+                   {"http": "http://192.168.0.100:3234"},
+                   {"http": "http://192.168.0.101:3234"},
+                   {"http": "http://192.168.0.102:3234"},
+                   {"http": "http://192.168.0.103:3234"}]
+        while True:
+            try:
+                response = requests.get(url=url, headers=headers, proxies=proxies[random.randint(0, 9)])
+                break
+            except Exception as e:
+                print(e)
+        try:
+            info = response.text.replace(' ', '')
+            link = re.search('href="(http://www\.nbd\.com\.cn/columns/298\?last_article=\d+&version_column=v5)', info).group(1)
+            print(link)
+            re_link = re.compile('(http://www\.nbd\.com\.cn/articles/\d+-\d+-\d+/\d+.html)')
+            articlelink = re_link.findall(info)
+            articlelinks = set(articlelink)
 
-        response = requests.get(url=url, headers=headers)
-        info = response.text.replace(' ', '')
-        link = re.search('href="(http://www\.nbd\.com\.cn/columns/298\?last_article=\d+&version_column=v5)', info).group(1)
-        print(link)
-        re_link = re.compile('(http://www\.nbd\.com\.cn/articles/\d+-\d+-\d+/\d+.html)')
-        articlelink = re_link.findall(info)
-        articlelinks = set(articlelink)
+            re_imglink = re.compile('(http://image\.nbd\.com\.cn/uploads/articles/thumbnails/.*?\.jpg)')
+            re_imglinks = re_imglink.findall(info)
 
-        re_imglink = re.compile('(http://image\.nbd\.com\.cn/uploads/articles/thumbnails/.*?\.jpg)')
-        re_imglinks = re_imglink.findall(info)
-
-        re_read = re.compile('(\d+?)阅读')
-        re_reads = re_read.findall(info)
+            re_read = re.compile('(\d+?)阅读')
+            re_reads = re_read.findall(info)
+        except Exception as e:
+            print(e)
 
         meijin = Meijing()
         for i, j, k in zip(articlelinks, re_imglinks, re_reads):
@@ -163,13 +181,16 @@ def more(url):
                            {"http": "http://192.168.0.100:3234"},
                            {"http": "http://192.168.0.101:3234"},
                            {"http": "http://192.168.0.102:3234"},
-                           {"http": "http://192.168.0.103:3234"}, ]
+                           {"http": "http://192.168.0.103:3234"} ]
                 headers = {
                     'Accept': 'text / html, application / xhtml + xml, application / xml;q = 0.9, image / webp, image / apng, * / *;q = 0.8',
                     'Accept - Encoding': 'gzip, deflate',
                     'Accept - Language': 'zh - CN, zh;q = 0.9',
                     'Cache - Control': 'max - age = 0',
-                    'Connection': 'keep - alive'
+                    'Connection': 'keep - alive',
+                    'Host':'www.nbd.com.cn',
+                    'Upgrade-Insecure-Requests': '1',
+                    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.146 Safari/537.36'
                 }
                 while True:
                     try:
@@ -198,9 +219,9 @@ def more(url):
                         file_name = j
                         j = qiniufetch(j, file_name)
                         article.title_img = j
-                    except:
-                        article.desc = ''
-                        article.title_img = ''
+                    except Exception as e:
+                        print(e)
+
                     article.title = title
                     article.source_detail = source
                     article.post_time = time
@@ -211,8 +232,12 @@ def more(url):
                     article.url = i
                     article.crawler_time = datetime.datetime.now()
                     article.insert_db()
+
+
+
                 except Exception as e:
                     print(e)
+
         more(link)
     except Exception as e:
         print(e)
