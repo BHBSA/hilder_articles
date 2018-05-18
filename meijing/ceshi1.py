ce09fdf999388833afa175773eb04220375d433b
zjx <zjx@fangtianxiang.com>
2018-05-18 11:18:12 +0800

    'new'

---
+++ meijing/ceshi1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2018/5/17 13:05
+# @Author  : Alex
+# @Email   : zhangjinxiao@fangjia.com
+# @File    : ceshi1.py
+# @Software: PyCharm
+
+
+
+
---
+++ meijing_start.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2018/5/18 10:39
+# @Author  : Alex
+# @Email   : zhangjinxiao@fangjia.com
+# @File    : meijing_start.py
+# @Software: PyCharm
