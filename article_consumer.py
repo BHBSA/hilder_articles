d5e8470d7ec68f0b7ccf855d96fc492d51ecb3b4
chengjiwen <chengjiwen@fangjia.com>
2018-05-18 16:38:50 +0800

    'consume'

--- article_consumer.py
+++ article_consumer.py
@@ -3,13 +3,16 @@ from wangyi.wangyi_consumer import WangyiConsumer
 from weixin.weixin_crawler import weixin_start
 from fangtianxia.fangtianxia_start import fangtianxia_start
 from meijing.meijing_start import meijing_start
-# from multiprocessing import Process
+from multiprocessing import Process
 from threading import Thread
+
+
 if __name__ == '__main__':
     con = Consumer()
     wangyi = WangyiConsumer()
-    Thread(target=con.start_consume).start()
-    Thread(target=wangyi.start_consume).start()
+    Process(target=con.start_consume).start()
+    Process(target=wangyi.start_consume).start()
     Thread(target=weixin_start).start()
     Thread(target=fangtianxia_start).start()
     Thread(target=meijing_start).start()
+
