06fb175eca7208eeecc5cc4e223352c9c91cdad0
jijunyu <jijunyu@fangjia.com>
2018-06-01 10:26:49 +0800

    test

--- test.py
+++ test.py
@@ -1,20 +1,5 @@
-import re
-import requests
-import chardet
-from readability.readability import Document
+from article_list_page.crawler_list_page_url import CrawlerArticleListUrl
 
-res = requests.get(
-    'https://m.fang.com/zhishi/xf/qg_341544.html?sf_source=ttcollaborate&tt_group_id=6521592606474895885')
-
-html_byte = re.sub(b'<script.*script>', b'', res.content,)
-
-# print(chardet.detect(html_byte))
-
-html = html_byte.decode(chardet.detect(html_byte)['encoding'])
-
-
-readable_title = Document(html).short_title()
-readable_article = Document(html).summary()
-
-print(readable_title)
-print(readable_article)
+if __name__ == '__main__':
+    c = CrawlerArticleListUrl()
+    c.crawler_url()
